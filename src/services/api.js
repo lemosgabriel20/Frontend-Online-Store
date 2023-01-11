9ce0d008403355df7475a865cb352581e41a0519
Marlon Couto <marloncouto.dev@gmail.com>
2023-01-11 16:32:28 -0300

    Implementa as requisições à API

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,21 @@
+const fetchCategories = async () => {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await response.json();
+  return data;
+};
+
+const fetchProductsFromCategory = async (categoryId, query) => {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const data = await response.json();
+  return data;
+};
+
 export async function getCategories() {
-  // Implemente aqui
+  return fetchCategories();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  return fetchProductsFromCategory(categoryId, query);
 }
 
 export async function getProductById() {
